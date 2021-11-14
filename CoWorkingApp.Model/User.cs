7cc52ad82ebd6d12330c510ff3c6b0013fe6980e
nhuamani <nhuamani.c7@gmail.com>
2021-11-13 19:34:28 -0800

    Refactorin

--- CoWorkingApp.Model/User.cs
+++ CoWorkingApp.Model/User.cs
@@ -9,9 +9,5 @@ namespace CoWorkingApp.Model
         public String LastName { get; set; }
         public String Email { get; set; }
         public String Password { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 8c22884e466ade7870c238b5229b25792edb411a
     }
 }
